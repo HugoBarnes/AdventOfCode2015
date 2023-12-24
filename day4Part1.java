8349e073eb439a2d4231e875b5c2c38a32169327
HugoBarnes <hugo.s.barnes@gmail.com>
2023-12-24 13:28:13 -0500

    added comments for clarity

--- day4Part1.java
+++ day4Part1.java
@@ -9,33 +9,33 @@ public class day4Part1 {
     // if it doesn't have enough leading zeroes (5)
     // increase the concatenation
     public static void main(String[] args) {
-        String input = "ckczppom";
-        int targetZeroes = 5;
-        int number =0;
+        String input = "ckczppom"; // add numbers to this string to get its MD5 hash beginning with 5 zeroes
+        int targetZeroes = 5; // target zeroes; 5
+        int number =0; // number that we will add
 
-        while(true){
-            String toHash = input+number;
-            String hash = getMd5Hash(toHash);
+        while(true){ // do this until we break
+            String toHash = input+number; // concatenate the string with our number
+            String hash = getMd5Hash(toHash); // Md5 hash the string
 
-            if(hash.startsWith(String.format("%0"+ targetZeroes+ "d", 0))){
-                System.out.println("Number found " + number);
-                System.out.println("Hash: " + hash);
-                break;
+            if(hash.startsWith(String.format("%0"+ targetZeroes+ "d", 0))){ // if hash starts with 5 zeroes:
+                System.out.println("Number found " + number); // display number
+                System.out.println("Hash: " + hash); // display hash
+                break; // leave the for loop
             }
-            number++;
+            number++; // otherwise, increase number
         }
     }
     private static String getMd5Hash(String input){
-            try{
-                MessageDigest md =MessageDigest.getInstance("MD5");
-                byte[] MessageDigest = md.digest(input.getBytes());
-                BigInteger no = new BigInteger(1,MessageDigest);
-                String hashtext = no.toString(16);
-                while(hashtext.length() <32){
+            try{ // try to call MD5 algo
+                MessageDigest md =MessageDigest.getInstance("MD5"); // call MD5 algo
+                byte[] MessageDigest = md.digest(input.getBytes()); // hash input string and save it into a byte array
+                BigInteger no = new BigInteger(1,MessageDigest); // Convert hash byte array into a big integer ; 1 is positive signum
+                String hashtext = no.toString(16);  // Convert bigInteger to a hexadecimal string
+                while(hashtext.length() <32){ // add leading zeroes if the hash is shorter than 32 characters
                     hashtext = "0" + hashtext;
                 }
-                return hashtext;
-            } catch(NoSuchAlgorithmException e){
+                return hashtext; // return the hash 
+            } catch(NoSuchAlgorithmException e){ // MD5 algorithm is no good; throw error
                 throw new RuntimeException(e);
             }
     }
